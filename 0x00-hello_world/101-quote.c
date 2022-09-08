2dadc36e0f8b175aae333e8a3284fa30722549ac
koleladan <isaiahodan@gmail.com>
2022-09-08 16:06:47 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,11 @@
  * main - main block
  * Return: 1
  */
+int main(void)
 {
 	int s =strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	write(2,
 			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			s);
-	return (1)
+	return (1);
 }
