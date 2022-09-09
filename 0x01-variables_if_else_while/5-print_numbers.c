f6743ef4542d3f71e3cae5edfb788ec52b6becf9
koleladan <isaiahodan@gmail.com>
2022-09-09 13:45:36 -0700

    basez

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /** 
  * main - printing digits from 0 to 10
  *
@@ -8,7 +9,7 @@ int main(void)
 {
 	int num;
 
-	for ( num = 0; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		printf("%d", num);
 
 	printf("\n");
