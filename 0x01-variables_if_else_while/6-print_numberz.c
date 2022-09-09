19db95ab3bbb63ee8b776b26f87ac32c2b0ad3fd
koleladan <isaiahodan@gmail.com>
2022-09-09 14:14:20 -0700

    char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - print all single digit numbers base from 0 to 10 only using putchar and without char variables
+ * main - print all single digit numbers base from 0 to 10
+ * only using putchar without char variables
  *
  * REturn: Always 0(success)
  */
