639c018fe588bf4770aae7cfc9d3fcda7b54d8b1
koleladan <isaiahodan@gmail.com>
2022-09-15 08:49:00 -0700

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_ICLUDED
+
+#include <stdio.h>
+
+
+
+void positive_or_negative(int i);
+largest = largest_number(a, b, c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif // MAIN_H_INCLUDED
