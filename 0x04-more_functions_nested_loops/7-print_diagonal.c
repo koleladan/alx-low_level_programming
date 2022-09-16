16802f46a4812e81bd403d29f92203c543d3c4e1
koleladan <isaiahodan@gmail.com>
2022-09-16 09:42:51 -0700

    main

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+
+
+/**
+ * print_diagonal - function
+ * @n: operand number
+ * Return: void
+ */
+
+void print_diagonal(int n)
+
+{
+
+	int line, spaces;
+
+
+
+	while (line < n && n > 0)
+
+	{
+
+		spaces = 0;
+
+		while (spaces < lines)
+
+		{
+
+			_putchar(' ');
+
+			spaces++;
+
+		}
+
+		_putchar('\\');
+
+		_putchar('\n');
+
+		lines++;
+
+	}
+
+	if (i <= 0)
+
+	{
+
+		_putchar('\n');
+
+}
