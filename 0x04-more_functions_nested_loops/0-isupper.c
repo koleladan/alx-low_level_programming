832e4051cf3fc8a083c73874b2bbf56d68ad62a5
koleladan <isaiahodan@gmail.com>
2022-09-16 08:21:10 -0700

    isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,13 @@
 
 /**
  * _isupper - checks for uppercase
- * @c: character to checked for case
- *
- * Return: 1 if uppercase, 0 if not uppercase
+ * @c: is an operand number
+ * checks if a character is uppercase
+ * Return: 1 if uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	return (0);
 }
