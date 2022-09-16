9617a1a2a5a5a4d4490ae4385476fb63eec9fa25
koleladan <isaiahodan@gmail.com>
2022-09-16 08:35:34 -0700

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-#include "main.h"
-#include <stdio.h>
-
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,4 +9,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
