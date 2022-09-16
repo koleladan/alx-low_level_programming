68d5f9f59ba205555404f7c0afcbbeb558e867f2
koleladan <isaiahodan@gmail.com>
2022-09-16 09:12:04 -0700

    main

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+
+/**
+ * print_most_numbers - print all numbers but 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+
+{
+
+	int num;
+
+
+
+	for (num = 0; num < 10; num++)
+
+	{
+
+		if (num != 2 && num != 4)
+
+			_putchar(num + '0');
+
+	}
+
+	_putchar('\n');
+
+}
