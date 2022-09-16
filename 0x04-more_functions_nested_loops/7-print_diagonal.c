51f6fcce917b14b93e70be80eba09771637f978e
koleladan <isaiahodan@gmail.com>
2022-09-16 10:38:57 -0700

    main

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,27 +11,39 @@
 void print_diagonal(int n)
 
 {
+
 	int line, spaces;
+
+
 
 	while (line < n && n > 0)
 
 	{
+
 		spaces = 0;
 
 		while (spaces < lines)
 
 		{
+
 			_putchar(' ');
+
 			spaces++;
+
 		}
+
 		_putchar('\\');
 
 		_putchar('\n');
 
 		lines++;
+
 	}
+
 	if (i <= 0)
+
 	{
+
 		_putchar('\n');
 	}
 
