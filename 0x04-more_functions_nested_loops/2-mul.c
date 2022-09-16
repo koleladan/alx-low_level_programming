f71b0a91e7d11ed6331cdaff1bfcc3fb9dc781b8
koleladan <isaiahodan@gmail.com>
2022-09-16 09:02:55 -0700

    main

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * mul - function
+ * @a: is an operand number
+ * @b: is an operand number
+ * Return: sum
+ */
+
+int mul(int a, int b)
+
+{
+
+	long int product;
+
+
+
+	product = a * b;
+
+	return (product);
+
