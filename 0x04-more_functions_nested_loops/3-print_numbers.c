c3beee72ef4d4dba2774886c89b3668c020fda08
koleladan <isaiahodan@gmail.com>
2022-09-16 09:09:27 -0700

    main

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+ * print_numbers - funtion
+  * Return: void
+ */
+
+void print_numbers(void)
+
+{
+
+	int num;
+
+
+
+	for (num = 0; num < 10; num++)
+
+	{
+
+		_putchar(num + '0');
+
+	}
+
+	_putchar('\n');
+
+}
