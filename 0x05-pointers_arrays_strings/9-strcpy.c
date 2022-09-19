e1865fbcf2e818d77cf72667fd6be748ec3d4fd9
koleladan <isaiahodan@gmail.com>
2022-09-19 13:34:36 -0700

    main

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ * _strcpy - function to copy a string to another
+ * @dest: string to be copied to / replaced
+ * @src: string to be copied
+ * Return: *dest
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+
+	strcpy(dest, src);
+
+	return (dest);
+
+}
