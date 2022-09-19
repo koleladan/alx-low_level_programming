a5ac0144f686a40762bd59861cf9f9b008823bfa
koleladan <isaiahodan@gmail.com>
2022-09-19 13:39:37 -0700

    main

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+
+
+/**
+ * _atoi - function to convert string to integer
+ * @s: input string
+ * Return: converted integer
+ */
+
+int _atoi(char *s)
+
+{
+
+	int num;
+
+
+
+	num = atoll(s);
+
+	return (num);
+
+}
