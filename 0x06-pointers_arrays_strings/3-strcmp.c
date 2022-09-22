29c97398da16e4208e5bb6d536bc53f03ab40a86
koleladan <isaiahodan@gmail.com>
2022-09-22 11:58:11 -0700

    main

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ * _strcmp - check if two strings are the same
+ * @s1: first string
+ * @s2: 2nd string
+ * Return: less than 0 if , 0 if the same and > 0 if
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+	int a;
+
+
+
+	a = strcmp(s1, s2);
+
+
+
+	return (a);
+
+}
