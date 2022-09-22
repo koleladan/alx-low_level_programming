db1a27507d6ed561c532385bc220d65079bc708a
koleladan <isaiahodan@gmail.com>
2022-09-22 11:51:15 -0700

    main

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: added on string
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+	dest = strcat(dest, src);
+
+
+
+	return (dest);
+
+}
