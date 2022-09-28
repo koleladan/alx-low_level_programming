d1d3358550ad7658e6f415e290a6504ced00550e
koleladan <isaiahodan@gmail.com>
2022-09-28 07:19:08 -0700

    trial

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints a string, followed by a new line
+ * puts - function that output strings
+ * @s: the char
+ * Return: always 0
+ */
+
+void _puts_recursion(char *s);
+{
+	char s[] = "_recursion";
+
+	puts("_recursion");
+	puts("\n");
+
+	retun 0;
+}
