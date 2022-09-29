c237003a1db48e4bcc462da586667f310fdfd41a
koleladan <isaiahodan@gmail.com>
2022-09-29 08:52:45 -0700

    main

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,56 +3,60 @@
 
 
 /**
- * check_prime - checks if an integer is prime
- * @n: number to be checked
- * @c: number used for check
- * Return: 1 or 0
+ * sqrt_evaluate - checks if a number is a perfect square
+ * @i: number used for checking
+ * @n: number to be checked:
+ * Return: number if square root
  */
 
 
 
-int check_prime(unsigned int n, unsigned int c)
+int sqrt_evaluate(int i, int n)
 
 {
 
-	if (n % c == 0)
+	if (i * i == n)	/* condition base */
 
-	{
+		return (i);
 
-		if (n == c)
+	else if (i * i > n)	/* return -1 since n is not a perfect square */
 
-			return (1);
+		return (-1);
 
-		else
-
-			return (0);
-
-	}
-
-	return (0 + check_prime(n, c + 1));
+	return (0 + sqrt_evaluate(i + 1, n));
 
 }
 
 
 
 /**
- * is_prime_number - checks if an integer is prime
- * @n: number to be checked
- * Return: 1 if prime else return 0
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: number whose square root is to be found
+ * Return: square root, or -1
  */
 
 
 
-int is_prime_number(int n)
+int _sqrt_recursion(int n)
 
 {
 
-	if (n <= 1)
+	int i;
 
-		return (0);
 
-	else
 
-		return (check_prime(n, 2));
+	i = 2;
+
+	if (n < 0)	/* Return value if n is negative */
+
+		return (-1);
+
+	else if (n == 0 || n == 1)	/* base case n is 1 or 0 */
+
+		return (n);
+
+	else	/* Recursive call to find the square root */
+
+		return (sqrt_evaluate(i, n));
 
 }
