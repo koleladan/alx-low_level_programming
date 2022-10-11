931e5374b27605ba2f0abb7ffe6045a247904379
koleladan <isaiahodan@gmail.com>
2022-10-11 10:05:07 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+
+#ifndef SIZE
+#define SIZE (1024)	/* declaring macro in a header file */
+#endif /* SIZE */
+
+
+
+typedef int make_compiler_happy;
+
+
+
+#endif /* OBJECT_LIKE_MACRO_H */
