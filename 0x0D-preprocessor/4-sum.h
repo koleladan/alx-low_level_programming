f48b8801f082a6ff577ac947079bbc4dae008287
koleladan <isaiahodan@gmail.com>
2022-10-12 00:05:24 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,19 @@
+#ifndef SUM_H
+
+#define SUM_H
+
+
+
+#ifndef SUM
+
+#define SUM(x, y) ((x) + (y))	/*macro to find the sum of 2 numbers*/
+
+#endif /* SUM */
+
+
+
+typedef int make_compiler_happy;
+
+
+
+#endif /*SUM_H*/
