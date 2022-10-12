ab3234c4e7abf45b2cc3812fc3f98897261098d0
koleladan <isaiahodan@gmail.com>
2022-10-12 00:03:29 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,18 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+
+
+#ifndef ABS
+
+#define ABS(x) ((x) < (0) ? (x) * (-1) : (x))
+
+#endif /* ABS */
+
+
+
+typedef int make_compiler_happy;
+
+
+
+#endif /* FUNCTION_LIKE_MACRO_H */
