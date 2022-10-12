d5edd66cccd4102d9bcc974f37f09e56f6ac2469
koleladan <isaiahodan@gmail.com>
2022-10-11 23:57:52 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,18 @@
+#ifndef PI_H
+#define PI_H
+
+
+
+#ifndef PI
+
+#define PI 3.14159265359	/*define type float macro from header file*/
+
+#endif /* PI */
+
+
+
+typedef float make_compiler_happy;
+
+
+
+#endif /*PI_H*/
